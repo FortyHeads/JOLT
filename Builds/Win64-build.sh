f7dcda31af2f35d9bb7d895927186cc76e858dca
FortyHeads Developer <dev@fortyheads.com>
2017-05-04 17:26:56 +0300

    added windows build scripts

---
+++ Builds/Win64-build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+	sudo apt-get install build-essential libtool autotools-dev automake pkg-config bsdmainutils curl
+
+	sudo apt-get install g++-mingw-w64-x86-64 mingw-w64-x86-64-dev
+	cd ..
+	cd depends
+	make HOST=x86_64-w64-mingw32 -j 8
+
+	cd sources
+	if test -f "libsodium-1.0.8-mingw.tar.gz"; then
+  		echo libsodium-1.0.8-mingw.tar.gz: yes; 
+	else
+		wget  https://download.libsodium.org/libsodium/releases/old/libsodium-1.0.8-mingw.tar.gz;
+	fi
+	tar -xvzf libsodium-1.0.8-mingw.tar.gz  libsodium-win64
+
+	cp -a libsodium-win64/* ./../x86_64-w64-mingw32
+	rm -rf libsodium-win64 
+	cd ..
+	cd ..
+	./autogen.sh
+	CONFIG_SITE=$PWD/depends/x86_64-w64-mingw32/share/config.site ./configure --prefix=/ --disable-tests
+	make -j 8 		
+exit 0
